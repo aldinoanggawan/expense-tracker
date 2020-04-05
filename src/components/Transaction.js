0113a46168b2203791df502a841a0ae58eedc781
Aldino Dwi Anggawan <aldinoanggawan@gmail.com>
2020-04-05 21:11:39 +0800

    Added function to delete each list

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext)
+
     const sign = transaction.amount > 0 ? "+" : "-"
     const indicator = transaction.amount > 0 ? "plus" : "minus"
+
+    const handleClick = () => {
+        deleteTransaction(transaction.id)
+    }
+
     return(
         <li className={indicator}>
             {transaction.text}
             <span>{sign}${Math.abs(transaction.amount)}</span>
-            <button className="delete-btn">x</button>
+            <button className="delete-btn" onClick={handleClick}>x</button>
         </li>
     )
 }
